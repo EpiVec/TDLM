06c7e3238bcb41798f451f4a478aca0d2bc188f2
maximelenormand <maxime.lenormand@inrae.fr>
2023-02-07 14:13:13 +0100

    test rJava

--- R/helloJavaWorld.R
+++ R/helloJavaWorld.R
@@ -1,3 +1,12 @@
+#' hello
+#' 
+#' @return
+#' hello 
+#' 
+#' @examples
+#' helloJavaWorld()
+#' 
+#' @export
 helloJavaWorld <- function(){
    hjw <- .jnew("HelloJavaWorld") # create instance of HelloJavaWorld class
    out <- .jcall(hjw, "S", "sayHello") # invoke sayHello method
