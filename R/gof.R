77aa6405f4f1b168f9537c7daaa0b245c8d9ec00
maximelenormand <maxime.lenormand@inrae.fr>
2023-03-15 08:52:53 +0100

    Update gof & vignette

--- R/gof.R
+++ R/gof.R
@@ -412,9 +412,12 @@ gof <- function(sim, obs, measures = "all", distance = NULL, bin_size = 2,
   # Initialize res
   if (tdlm) {
     if (hlist == 2) {
+      paramval <- sim$info[-c(1:4),2]
+      paramval <- merge(names(sim[[2]]),paramval)[,2]
       nameres <- merge(names(sim[[2]]),names(sim)[-1])
       nameres <- nameres[,c(2,1)]
-      res <- data.frame(Parameter = nameres[, 1], 
+      res <- data.frame(Parameter = nameres[, 1],
+                        Parameter_value = paramval,
                         Simulation = nameres[, 2])
     } else {
       res <- data.frame(Simulation = names(sim)[-1])
